27b149c7c4223257f67f0ea590d402606db4a6a8
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2020-11-05 09:41:34 +0700

    Create User Model for backend

---
+++ auth/src/main/java/com/backend/auth/model/User.java
@@ -0,0 +1,13 @@
+package com.backend.auth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private int id;
+    private String name, email, mobile;
+}
